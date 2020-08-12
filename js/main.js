8d92d926f64d277b5e24c8fd6ceaaaeffc420398
Md Ashik <mostofabilla@gmail.com>
2020-08-12 23:18:59 +0600

    main js added.

--- js/main.js
+++ js/main.js
@@ -1,56 +1,47 @@
-// $(document).ready(function () {
-
-// // main js Start
-//   // type your code 
-// // main js end
-// });
-
-
-$(window).scroll(function(){
-  $('header').toggleClass('scrolled', $(this).scrollTop() > 100);
+$(window).scroll(function () {
+    $('header').toggleClass('scrolled', $(this).scrollTop() > 100);
 });
 
-$(window).scroll(function(){
-  $('.navbar-nav li').toggleClass('scrolled', $(this).scrollTop() > 100);
+$(window).scroll(function () {
+    $('.navbar-nav li').toggleClass('scrolled', $(this).scrollTop() > 100);
 });
 
 
-var loader = document.getElementById('preloader');
-function preLoader(){
-  loader.style.display = 'none';
+const loader = document.getElementById('preloader');
 
+function preLoader() {
+    loader.style.display = 'none';
 }
 
 $('.dot-list').owlCarousel({
-  loop:true,
-  autoplayHoverPause:true,
-  margin: 30,                              
-  nav:false,
-  autoplay: true,
-  dots: true,
-  smartSpeed:1000,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:2
-      },
-      700:{
-          items:2
-      },
-      1000:{
-          items:3
-      }
-  }
+    loop: true,
+    autoplayHoverPause: true,
+    margin: 30,
+    nav: false,
+    autoplay: true,
+    dots: true,
+    smartSpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        700: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
 });
 
-jQuery(document).ready(function( $ ){
-  $('.counter').counterUp({
-      delay: 10,
-      time: 1000
-  });
-
-
-});
+// jQuery(document).ready(function ($) {
+//     $('.counter').counterUp({
+//         delay: 10,
+//         time: 1000
+//     });
+//
+// });
 
