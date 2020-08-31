6c7910c5e2e337310e01d3d066ffa4335a496a46
unknown <you@example.com>
2020-08-31 12:44:13 +0600

    smooth scrolling done

--- js/main.js
+++ js/main.js
@@ -89,3 +89,18 @@ $.scrollUp({
     animation: 'fade'
 });
 
+$('.navbar a').on('click', function(e){
+    if(this.hash !== ''){
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+        },
+        1500
+
+        );
+    }
+});
