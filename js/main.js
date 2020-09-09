8c83d898983c14a4932c26efba04815e1a0c4ddf
Subarna Akter <subarna769@gmail.com>
2020-09-09 19:53:41 +0600

    full project completed

--- js/main.js
+++ js/main.js
@@ -49,36 +49,7 @@ $('.dot-list').owlCarousel({
     }
 });
 
-//counter
 
-$(window).on("load scroll",function(){
-    startCounting();
-});
-
-function startCounting() {
-    if (isElementVisible($('.counter')) && !countingFinish) {
-        countingFinish = true;
-        $('.counter').each(function (e) {
-            $(this).prop('Counter', 0).animate({
-                Counter: $(this).text()
-            }, {
-                duration: 4000,
-                easing: 'swing',
-                step: function (now) {
-                    $(this).text(Math.ceil(now));
-                }
-            });
-        });
-    }
-}
-
-function isElementVisible($elementToBeChecked) {
-    const TopView = $(window).scrollTop();
-    const BotView = TopView + $(window).height();
-    const TopElement = $elementToBeChecked.offset().top;
-    const BotElement = TopElement + $elementToBeChecked.height();
-    return ((BotElement <= BotView) && (TopElement >= TopView));
-}
 
 //scrollup
 
